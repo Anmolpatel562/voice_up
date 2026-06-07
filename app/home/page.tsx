25e21e6dfa3da94888ed91e1d70a4e364a55f8a3
Anmol Patel <anmolpatel562@gmail.com>
2026-06-07 13:16:44 +0530

    auth: google oauth working with prisma adapter

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { useSession } from "next-auth/react"
+import { useEffect } from "react"
+
+
+
+export default function home() {
+    return (
+        <div>Welome to home page</div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 "use client"
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("session => ", session);
+    if (status == 'authenticated') {
+      router.push('/home');
+    }
+  }, [status, router]);
+
+  return ( 
     <main>
       <h1>VoiceUp</h1>
       <p>Feel Confident While Speaking in English with VoiceUp</p>
